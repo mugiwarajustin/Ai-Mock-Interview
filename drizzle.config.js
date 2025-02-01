3bde74c3c5b51391cb535a1f88759e604527480c
Ritesh <shivay.lfc@gmail.com>
2025-02-01 15:05:28 +0530

    all

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,6 +3,6 @@ export default {
     schema: "./utils/schema.js",
     dialect: 'postgresql',
     dbCredentials: {
-        url: 'postgresql://ai%20mock%20interview_owner:aQ7GxFrR2qHj@ep-quiet-hat-a5sn0e5z.us-east-2.aws.neon.tech/ai-mock-interview?sslmode=require',
+        url: 'postgresql://neondb_owner:npg_BPkV2Mt4WbiG@ep-shy-cloud-a8goicja-pooler.eastus2.azure.neon.tech/neondb?sslmode=require',
     }
 };
